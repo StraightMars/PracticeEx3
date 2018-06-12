f4799ca396e059e7e950fa0c4887ee33c7a7a213
nureevmarsel <mnurev@mail.ru>
2018-06-12 16:59:26 +0500

    first commit

---
+++ Practice3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok;
+            double x, y;
+            bool result;
+            Console.WriteLine("Введите абсциссу точки: ");
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out x);
+                if (!ok)
+                    Console.WriteLine("Ошибка ввода. Введите действительное число.");
+            } while (!ok);
+            Console.WriteLine("Введите ординату точки: ");
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out y);
+                if (!ok)
+                    Console.WriteLine("Ошибка ввода. Введите действительное число.");
+            } while (!ok);
+            if (Math.Abs(x) <= 1 && Math.Abs(y) <= 1)
+                result = true;
+            else
+                result = false;
+            Console.WriteLine(result);
+        }
+    }
+}
